5414cfba43b66a633a090512b3abfc23bf8c98b5
Jacob Stephenson <jacobstephenson97@gmail.com>
2022-11-10 17:21:44 -0500

    Added Query for event page

--- src/server/trpc/router/event.ts
+++ src/server/trpc/router/event.ts
@@ -4,16 +4,19 @@ import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const eventRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
+  getEvent: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(({ input, ctx }) => {
+      return ctx.prisma.event.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
   eventCreate: publicProcedure
     .input(
       z.object({
